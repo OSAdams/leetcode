834ff0086a5d12282f36c49f80cd12062a85b30b
Owen Adams <owenadams.main@gmail.com>
2024-05-24 17:35:07 +0000

    [LeetCode Sync] Runtime - 47 ms (86.29%), Memory - 48.7 MB (82.61%)

--- my-folder/0088-merge-sorted-array/solution.js
+++ my-folder/0088-merge-sorted-array/solution.js
@@ -6,14 +6,14 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let i = m - 1;
-    let j = n - 1;
-    let k = m + n - 1;
-    while (j >= 0) {
-        if (i >= 0 && nums1[i] > nums2[j]) {
-            nums1[k--] = nums1[i--];
+    let nums1Pointer = m - 1;
+    let nums2Pointer = n - 1;
+    let currentPointer = m + n - 1;
+    while (nums2Pointer >= 0) {
+        if (nums1Pointer >= 0 && nums1[nums1Pointer] > nums2[nums2Pointer]) {
+            nums1[currentPointer--] = nums1[nums1Pointer--]
         } else {
-            nums1[k--] = nums2[j--];
+            nums1[currentPointer--] = nums2[nums2Pointer--]
         }
-    }
+    } 
 };
