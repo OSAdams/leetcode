5439f4c23fb950c63f440e80768244a10590cfed
Owen Adams <owenadams.main@gmail.com>
2024-09-03 13:38:08 +0000

    [LeetCode Sync] Runtime - 49 ms (94.00%), Memory - 52.6 MB (13.56%)

---
+++ my-folder/1894-merge-strings-alternately/solution.ts
@@ -0,0 +1,13 @@
+function mergeAlternately(word1: string, word2: string): string {
+    const maxLength: number = Math.max(word1.length, word2.length);
+    let result: string = '';
+    for (let i = 0; i < maxLength; i++) {
+        if (word1[i]) {
+            result += word1[i]
+        }
+        if (word2[i]) {
+            result += word2[i]
+        }
+    }
+    return result;
+};
